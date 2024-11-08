3e92b9d5a875f46235ce1f47037aa08720a5040d
ahmatfauzy <ahmatf703@gmail.com>
2024-11-08 17:25:05 +0700

    update projects section

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-
 const tombol = document.getElementById("hamburger-menu");
 const menu = document.querySelector(".menu");
 
 tombol.addEventListener("click", () => {
   menu.classList.toggle("aktif");
 });
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Mencegah form dikirim secara default
+
+    var name = document.getElementById("name").value;
+    var message = document.getElementById("message").value;
+
+    // Menampilkan popup alert dengan pesan
+    alert("Terima kasih " + name + ", pesan sudah terkirim.");
+
+    // Reset form
+    document.getElementById("contactForm").reset();
+  });
